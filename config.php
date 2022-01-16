d326663f56161355276fddffc749033145c93be8
moxaPanchal <moxaa.panchal@gmail.com>
2022-01-16 09:44:17 -0500

    d

--- config.php
+++ config.php
@@ -4,7 +4,19 @@ This file contains database configuration assuming you are running mysql using u
 */
 // Try connecting to the database
 // $conn means connection
-$conn = mysqli_connect("localhost", "root", "", "login");
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+// $conn = mysqli_connect("localhost", "root", "", "login");
 
 // Check the connection
 if(!$conn){
